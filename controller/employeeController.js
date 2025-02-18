be7606c94f65332e827fda44d11bd85ea36af6f8
vamshithati <vthati@hawk.iit.edu>
2025-02-18 13:13:40 -0600

    implemented GET

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,4 +1,4 @@
-const Employee = require('../models/employee')
+const Employee = require('../models/Employee')
 const createEmployee=async(req, res)=>{
     try{
         const{ name, email, phone, city}=req.body
@@ -14,4 +14,13 @@ const createEmployee=async(req, res)=>{
     }
 }
 
-module.exports={createEmployee}
+const getEmployees=async(req,res)=>{
+    try{
+        const employees=await Employee.find()
+        res.status(200).json(employees)
+    }catch(error){
+        console.error("Error:", error)
+        res.status(500).json({message: 'server error'})
+    }
+}
+module.exports={createEmployee, getEmployees}
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const router=express.Router()
 const employeeController=require('../controller/employeeController')
-const employee=require('../models/employee')
+const Employee= require('../models/Employee')
+
 
 router.post('/add-emp', employeeController.createEmployee)
 
-
+router.get('/allemployees', employeeController.getEmployees)
 
 
 module.exports=router
