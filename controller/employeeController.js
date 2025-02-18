edc02e4efb0dd738b2d89357e955f5598a4d645d
vamshithati <vthati@hawk.iit.edu>
2025-02-17 23:03:37 -0600

    Controller to acces CRUD operations

---
+++ controller/employeeController.js
@@ -0,0 +1,17 @@
+const Employee=require('../models/employee')
+const createEmployee=async(req, res)=>{
+    try{
+        const{ name, email, phone, city}=req.body
+        const employee=new Employee({
+            name, email, phone, city
+        })
+        await employee.save()
+        res.status(201).json(employees)
+    }
+    catch{
+        console.log("error: ", error)
+        res.status(500).json({message:'server-error'})
+    }
+}
+
+module.exports={createEmployee}
