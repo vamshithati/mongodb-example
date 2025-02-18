35428896d375f594c41d78f4d762f72bea3f4582
vamshithati <vthati@hawk.iit.edu>
2025-02-17 23:04:09 -0600

    EnRouting CRUD operations

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const router=express.Router()
+const employeeController=require('../controller/employeeController')
+const employee=require('../models/employee')
+
+router.post('/add-emp',employeeController.createEmployee)
+
+
+
+
+module.exports=router
